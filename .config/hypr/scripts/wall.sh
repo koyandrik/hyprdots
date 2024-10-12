53c9f6c16786aced9b212306176c7740ba1e299b
koyandrik <aoshinkarev07@gmail.com>
2024-10-12 20:15:35 +0200

    hyprcfg

---
+++ .config/hypr/scripts/wall.sh
@@ -0,0 +1,25 @@
+#!/bin/env zsh
+
+hyprctl hyprpaper unload all
+
+wallpapers=($(ls -d /home/kdrik/wallpapers/*.jpg  2>/dev/null))
+
+
+wall=${wallpapers[$RANDOM % ${#wallpapers[@]}]}
+
+hyprctl hyprpaper preload "$wall"
+hyprctl hyprpaper wallpaper ,"$wall"
+
+
+source ~/myenv/bin/activate
+
+wpg -ns "$wall"
+
+wal -R
+
+active_border_color=$(sed -n '1p' ~/.cache/wal/colors)
+sed -i "s/col.active_border = .*/col.active_border = 0xff${active_border_color:5}/" ~/.config/hypr/cfg/general.conf
+
+hyprctl reload
+
+sleep 2
---
+++ .config/wpg/wp_init.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wpg -nrs '/home/kdrik/wallpapers/medium-shot-anime-characters-hugging(1).jpg' '/home/kdrik/wallpapers/medium-shot-anime-characters-hugging(1).jpg'
