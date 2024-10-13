4d76ee4f5a6d6c89ef9d166b175ce9a1cf98d49f
koyandrik <165103084+koyandrik@users.noreply.github.com>
2024-10-13 23:41:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 yay -S brightnessctl pipewire nautilus gnome-keyring qt5ct qt6ct xdg-desktop-portal-hyprland rofi cliphist grim cava playerctl python python-pywal ttf-jetbrains-mono-nerd bluez bluez-utils blueman waybar hyprpaper firefox
-cp -r .bashrc ~/.bashrc
-cp -r .bash_logout ~/.bash_logout
-cp -r .bash_profile ~/.bash_profile
-cp -r .local ~/.local
-cp -r .config ~/.config
+cp -r .bashrc ~/
+cp -r .bash_logout ~/
+cp -r .bash_profile ~/
+cp -r .local ~/
+cp -r .config ~/
 clear
 sleep 2
 echo "Welcome to my Hypr Setup"
