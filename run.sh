237e16ac936c1c0f8493b28aea9a4445fe6350cf
koyandrik <165103084+koyandrik@users.noreply.github.com>
2024-10-14 18:28:31 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ makepkg -si
 cd 
 cd hyprdots
 yay -S brightnessctl pipewire nautilus gnome-keyring qt5ct qt6ct xdg-desktop-portal-hyprland rofi cliphist grim cava playerctl python ttf-font-awesome fastfetch ttf-jetbrains-mono-nerd bluez bluez-utils blueman waybar hyprpaper firefox
-sudo pacman -S python-pywal
 cp -r .bashrc ~/
 cp -r .bash_logout ~/
 cp -r .bash_profile ~/
 cp -r .local ~/
 cp -r .config ~/
 cp -r wallpapers ~/
+cp -r myenv ~/
 wal -i ~/wallpapers/1.jpg
 clear
 sleep 2
