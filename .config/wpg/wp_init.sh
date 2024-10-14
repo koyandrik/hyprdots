0a1b06933fd62a01d74ed060d024c74c6734aa03
koyandrik <165103084+koyandrik@users.noreply.github.com>
2024-10-15 00:15:52 +0200

    re-build

---
+++ .config/wpg/wp_init.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wpg -rs '1.jpg' '1.jpg'
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd yay
 makepkg -si
 cd 
 cd hyprdots
-yay -S brightnessctl qt6ct pamixer nwg-look hyprlock wpgtk pywal wlogout pavucontrol nautilus gnome-keyring qt5ct qt6ct xdg-desktop-portal-hyprland libnotify rofi cliphist grim cava playerctl python ttf-font-awesome fastfetch ttf-jetbrains-mono-nerd bluez bluez-utils blueman waybar hyprpaper firefox
+yay -S brightnessctl nwg-look qt6ct pamixer nwg-look hyprlock wpgtk pywal wlogout pavucontrol nautilus gnome-keyring qt5ct qt6ct xdg-desktop-portal-hyprland libnotify rofi cliphist grim cava playerctl python ttf-font-awesome fastfetch ttf-jetbrains-mono-nerd bluez bluez-utils blueman waybar hyprpaper firefox
 cp -r .bashrc ~/
 cp -r .bash_profile ~/
 cp -r .local ~/
