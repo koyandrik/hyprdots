097b9623629ce6e3b062042c2c44e5df5a42bc67
koyandrik <165103084+koyandrik@users.noreply.github.com>
2024-10-24 00:48:36 +0200

    full re-model

--- .bashrc
+++ .bashrc
@@ -1,6 +1,8 @@
 #
 # ~/.bashrc
 #
+wal -R
+clear
 fastfetch
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
@@ -8,3 +10,5 @@ fastfetch
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
+
+export PATH=$PATH:/home/kdrik/.spicetify
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+#install dots
 cd 
 cd hyprdots
 yay -S brightnessctl python-pywalfox hyprshot nwg-look pamixer nwg-look hyprlock wpgtk pywal wlogout pavucontrol nautilus gnome-keyring xdg-desktop-portal-hyprland xdg-desktop-portal-gtk libnotify rofi cliphist grim cava playerctl python ttf-font-awesome fastfetch ttf-jetbrains-mono-nerd bluez bluez-utils blueman waybar hyprpaper firefox
@@ -9,18 +11,16 @@ cp -r .local ~/
 cp -r .config ~/
 cp -r wallpapers ~/
 cp -r myenv ~/
-wal -i ~/wallpapers/1.jpg
+wal -i ~/wallpapers/2.jpg
 clear
 sleep 2
 echo "Welcome to my Hypr Setup"
 sleep 1
-chmod +x re-name.sh
 chmod +x .local/share/bin/volumecontrol.sh
 chmod +x .local/share/bin/brightnesscontrol.sh
 chmod +x .local/share/bin/globalcontrol.sh
 sudo systemctl start bluetooth
 clear
-./re-name.sh
 /usr/lib/xdg-desktop-portal -rv
 clear
 sleep 1
